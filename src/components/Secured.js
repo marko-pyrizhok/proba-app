505b8913fa99cb87545524058f8dfa680b30c237
marko <public.marko.pyrizhok@gmail.com>
2020-04-25 20:21:55 +0300

    add tochka text

--- src/components/Secured.js
+++ src/components/Secured.js
@@ -1,46 +1,72 @@
 import React, { Component } from 'react';
 import {
-    ScrollView,
-    Text,
-    View,
-    Button
+  ScrollView,
+  Text,
+  View,
+  Button
 } from 'react-native';
+
+import '../style.css'
 
 export default class Secured extends Component {
 
-componentDidMount() {
+  state = {
+    probaResponceReceived: false,
+    probaPoints: ''
+  }
 
-    fetch("https://proba-api.herokuapp.com/my-proba", {
+  componentDidMount() {
+
+    fetch("https://proba-api.herokuapp.com/my-last-proba-points", {
       credentials: 'include',
       mode: 'cors',
-                method: "GET",
-                headers: {
-                    'Authorization': this.props.token
-                }
-            })
-            .then((response) => response.json())
-            .then((response) => {
-                if (response!=null) {
-                    console.log(response);
-                }
-            })
-            .catch(err => {
-			});
-}
+      method: "GET",
+      headers: {
+        'Authorization': this.props.token
+      }
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response != null) {
+          this.setState({ probaResponceReceived: true, probaPoints: response });
+        }
+      })
+      .catch(err => {
+      });
+  }
 
-    render() {
-        return (
-            <ScrollView style={{padding: 20}}>
-                <Text 
-                    style={{fontSize: 27}}>
-                    {this.props.token}
-                </Text>
-                <View style={{margin:20}} />
-                <Button
-                            onPress={this.props.onLogoutPress}
-                            title="Logout"
-                        />
-                </ScrollView>
-                )
-    }
+  render() {
+    return this.renderItem();
+  }
+
+  renderItem() {
+    return (
+      <ScrollView style={{ padding: 20 }}>
+        <View style={{
+          flex: 1,
+          width: 500,
+          height: 500,
+          justifyContent: 'space-between',
+        }}>
+          {
+            (
+              this.state.probaResponceReceived)
+              ? this.state.probaPoints.map((item) => {
+                return <View styleName='container' ><Text>{item.code} {item.name} {item.confirmUserId} {item.confirmDate}</Text></View>
+              })
+              : <View style={{
+                flex: 1,
+                width: 100,
+                height: 100,
+              }} />
+
+          }
+        </View>
+        <Button
+          onPress={this.props.onLogoutPress}
+          title="Logout"
+        />
+      </ScrollView>
+    )
+  }
 }
