3d5c8a7dd277f698fddda1acfb1154192bd24ce3
marko <public.marko.pyrizhok@gmail.com>
2020-11-09 01:07:16 +0200

    add custom font

--- App.jsx
+++ App.jsx
@@ -4,18 +4,36 @@ import { StatusBar } from 'react-native'
 import configureStore from './src/redux/store'
 import { initialiseApplication } from './src/redux/actions/application.actions'
 import Navigation from './src/routing'
+import { useFonts } from '@use-expo/font';
+import { AppLoading } from 'expo';
+
+import { Text, View } from 'react-native'
 
 const store = configureStore()
 store.dispatch(initialiseApplication())
 
-export default class App extends React.Component {
+export default function App() {
+  const [isLoaded] = useFonts({
+    "MyriadPro-Regular": require("./assets/fonts/MyriadPro-Regular.ttf"),
+    "MyriadPro-Bold": require("./assets/fonts/MyriadPro-Bold.ttf")
+  }); 
 
-  render() {
+    if (!isLoaded) {
+      return <AppLoading />;}
+      else {
     return (
       <Provider store={store}>
         <StatusBar hidden />
+        <Text 
+       style={{ fontFamily: "MyriadPro-Regular" }}>
+        Hello World
+     </Text><Text 
+       style={{ fontFamily: "MyriadPro-Bold" }}>
+        Hello World
+     </Text>
         <Navigation />
       </Provider>
     )
+      }
   }
-}
+
