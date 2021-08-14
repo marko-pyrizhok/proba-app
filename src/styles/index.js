6f5ea0a8bffe2ef6523b5256dff11f423faf4aec
marko <public.marko.pyrizhok@gmail.com>
2021-08-14 13:19:35 +0300

    minor formating for registration page

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,68 +1,66 @@
-import {
-    StyleSheet,
-} from 'react-native'
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    container: {
-        width: '100%',
-        height: '100%',
-        flex: 1,
-        alignItems: 'center',
-        backgroundColor: '#000',
-    },
-    containerCentered: {
-        width: '100%',
-        height: '100%',
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#000',
-    },
-    constrainer: {
-        flexGrow: 1,
-        alignItems: 'center',
-    },
-    constrainerCentered: {
-        flexGrow: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    headerPanel: {
-        flex: 1,
-        alignItems: 'center',
-    },
-    headline: {
-        flex: 1,
-        fontWeight: 'bold',
-    },
-    page: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    titleBox: {
-        width: '100%',
-        height: '50%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    contentBox: {
-        width: '100%',
-        height: '45%',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    footer: {
-        width: '100%',
-        height: '10%',
-        alignItems: 'flex-end',
-        justifyContent: 'center',
-    },
-    icon: {
-        width: 24,
-        height: 24,
-      },
-})
+  container: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: "#000",
+  },
+  containerCentered: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#000",
+  },
+  constrainer: {
+    flexGrow: 1,
+    alignItems: "center",
+  },
+  constrainerCentered: {
+    flexGrow: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  headerPanel: {
+    flex: 1,
+    alignItems: "center",
+  },
+  headline: {
+    flex: 1,
+    fontWeight: "bold",
+  },
+  page: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  titleBox: {
+    width: "100%",
+    height: "50%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  contentBox: {
+    width: "100%",
+    height: "45%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  footer: {
+    width: "100%",
+    height: "10%",
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  icon: {
+    width: 24,
+    height: 24,
+  },
+});
 
-export default styles
+export default styles;
